8d929fe47ada48969b493b8af006470cc1de6da5
marcoaureliomunhoz <marcoaureliomunhoz@gmail.com>
2017-12-16 18:57:20 -0200

    pcl_app_listview1

---
+++ solutions/pcl_app_listview1/pcl_app_listview1/pcl_app_listview1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pcl_app_listview1
+{
+    public partial class MainPage : ContentPage
+    {
+        private string[] _editoras = { "novatec", "Excel Books", "Brasport" };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            listEditoras.ItemsSource = _editoras;
+            listEditoras.ItemTapped += ListEditoras_ItemTapped;
+        }
+
+        private async void ListEditoras_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await DisplayAlert("Editora", e.Item.ToString(), "OK");
+            (sender as ListView).SelectedItem = null;
+        }
+    }
+}
