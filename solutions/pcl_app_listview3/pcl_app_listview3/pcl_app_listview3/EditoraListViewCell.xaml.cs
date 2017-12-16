4a64159ffc2d5543306278cfc72305757a3c2ca2
marcoaureliomunhoz <marcoaureliomunhoz@gmail.com>
2017-12-16 21:55:10 -0200

    pcl_app_listview3

---
+++ solutions/pcl_app_listview3/pcl_app_listview3/pcl_app_listview3/EditoraListViewCell.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pcl_app_listview3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditoraListViewCell : ViewCell
+    {
+        public EditoraListViewCell()
+        {
+            InitializeComponent();
+
+            lblNome.SetBinding(Label.TextProperty, nameof(Editora.Nome));
+            lblCodigo.SetBinding(Label.TextProperty, nameof(Editora.Codigo));
+            lblSite.SetBinding(Label.TextProperty, nameof(Editora.Site));
+
+            var detalhesAction = new MenuItem { Text = "Detalhes" };
+            detalhesAction.Clicked += DetalhesAction_Clicked;
+
+            var excluirAction = new MenuItem { Text = "Excluir", IsDestructive = true };
+            excluirAction.Clicked += ExcluirAction_Clicked;
+
+            ContextActions.Add(detalhesAction);
+            ContextActions.Add(excluirAction);
+        }
+
+        private void DetalhesAction_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Detalhes");
+        }
+
+        private void ExcluirAction_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Excluindo");
+        }
+    }
+}
---
+++ solutions/pcl_app_listview3/pcl_app_listview3/pcl_app_listview3/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pcl_app_listview3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            listViewEditoras.ItemsSource = new Editora[] {
+                new Editora
+                {
+                    Codigo = 1,
+                    Nome = "novatec",
+                    Site = "www.novatec.com.br"
+                },
+                new Editora
+                {
+                    Codigo = 2,
+                    Nome = "Excel Books",
+                    Site = "www.excelbooks.com"
+                }
+            };
+            listViewEditoras.ItemTemplate = new DataTemplate(typeof(EditoraListViewCell));
+        }
+    }
+}
